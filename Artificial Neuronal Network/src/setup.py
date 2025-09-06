f0db20d7435ca739c543da3c4f0f2371c0143571
Malte Scharf <malte.scharf@hotmail.de>
2025-09-06 22:01:25 +0200

    connections and weights added

--- Artificial Neuronal Network/src/setup.py
+++ Artificial Neuronal Network/src/setup.py
@@ -4,20 +4,24 @@
 # Class for Artifical Neural Network
 
 class NeuralNetwork():
-    values = []
-    weights = []
-    # matrix with connections between Neurons. Saves neuronIndex of next layer
-    connections = []
+
     def __init__(self, layers):
         self.layers = layers
+        self.values = []
+        # matrix with connections between Neurons. Saves neuronIndex of next layer
+        self.weights = []
+
+
 
         #initialzie values with None for layers
-        tempRow = []
-        for i in range(layers):
-            tempRow.append(0)  # TODO: Change zeros to None
-        self.values.append(tempRow)
-        self.weights.append(tempRow)
-        self.connections.append(tempRow)
+        tempRow = [0] * self.layers # TODO: Change zeros to None
+        self.values.append(tempRow.copy())
+
+        weightTemp = []
+        [weightTemp.append([]) for _ in range(self.layers)]
+        self.weights.append(weightTemp)
+
+
 
 
 
@@ -30,15 +34,19 @@ class NeuralNetwork():
         else:
             self.values[neuronIndex][layerIndex] = value
 
-    def addConnection(self,layerIndex,neuronIndex, nextNeuronIndex):
-        self.connections[layerIndex][neuronIndex] = nextNeuronIndex
+
+    def addWeight(self,layerIndex,neuronIndex, nextNeuronIndex, weight):
+        # Data structure: [nextNeuronIndex,weight]
+        self.weights[layerIndex][neuronIndex].append([nextNeuronIndex,weight])
 
 
     # Add Row to every matrix
     def addRow(self):
-        self.values.append([0,0,0])
-        self.weights.append([0,0,0])
-        self.connections.append([0,0,0])
+        self.values.append([0]* self.layers)
+
+        weightTemp = []
+        [weightTemp.append([]) for _ in range(self.layers)]
+        self.weights.append(weightTemp)
 
     # Print Methods
     # Useful Methods for information retrival and debugging
@@ -50,31 +58,45 @@ class NeuralNetwork():
         print("------------")
 
     def printConnections(self):
+        # TODO: Extract only connections from weights
         print("Connections:")
-        for i in range(len(self.connections)):
-            print(self.connections[i])
+
+    def printWeights(self):
+        print("Weights:")
+        for i in range(len(self.weights)):
+            print(self.weights[i])
         print("------------")
 
     def printNeuron(self,layerIndex, neuronIndex):
         print("Coordinate:",neuronIndex, layerIndex)
         print("Value:",self.values[neuronIndex][layerIndex])
-        print("Connections to:",self.connections[neuronIndex][layerIndex])
+        weightEntry = self.weights[neuronIndex][layerIndex];
+        [print("Connections to:",weightEntry[i][0],"with weight:",self.weights[neuronIndex][layerIndex][i][1]) for i in range(len(weightEntry))]
         # //TODO: add print functions for weight, connections etc.
+
+    # Setter
+
+    def setWeight(self,layerIndex, neuronIndex,nextNeuronIndex, newWeight):
+
+        cell = self.weights[neuronIndex][layerIndex]
+
+        # Search in cell [layerIndex, neuronIndex] for nextNeuron Index
+        match = [sub for sub in cell if sub[0] == nextNeuronIndex]
+        match[0][1] = newWeight
+
 
 
 
 def main():
     # create new NeuralNetwork
     ANN = NeuralNetwork(3)
-    ANN.printValues()
     ANN.addNeuron(0,3,1)
     ANN.addNeuron(1,2,2)
     ANN.addNeuron(0,0,3)
-    ANN.printValues()
-    ANN.printConnections()  # TODO: investigate why there is entry in connection matrix
+    ANN.addWeight(0,0,1,1.5)
+    ANN.addWeight(0,0,2,2.5)
+    ANN.setWeight(0,0,1,3.5)
     ANN.printNeuron(0,0)
-
-
 
 
 if __name__ == "__main__":
